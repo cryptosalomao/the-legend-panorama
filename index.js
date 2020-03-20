571088591a58888fbc5a942a8656993cba71f9c6
cryptosalomao <gsalomao_freela@hotmail.com>
2020-03-20 04:28:01 -0300

    Initial files

---
+++ index.js
@@ -0,0 +1,20 @@
+const PhotoSphereViewer = require('photo-sphere-viewer');
+
+const container = document.getElementById('viewer');
+
+const viewer = new PhotoSphereViewer({
+  container,
+  panorama: './img/1-andar.jpg',
+  navbar: [
+    'autorotate',
+    'zoom',
+  ],
+  time_anim: false,
+  latitude_range: [0, 245]
+});
+
+$('button.item').click(function(e) {
+  const nextImg = `./img/${$(e.target).data('andar')}.jpg`;
+  viewer.setPanorama(nextImg);
+  $(this).toggleClass('active').siblings().removeClass('active');
+});
